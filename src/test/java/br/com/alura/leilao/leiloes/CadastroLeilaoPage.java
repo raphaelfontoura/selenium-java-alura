b31a23d245d2cb0507530c6e3084891a6cc75191
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-11-17 23:38:48 -0300

    Finish test create new leilao with form

--- src/test/java/br/com/alura/leilao/leiloes/CadastroLeilaoPage.java
+++ src/test/java/br/com/alura/leilao/leiloes/CadastroLeilaoPage.java
@@ -1,5 +1,6 @@
 package br.com.alura.leilao.leiloes;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CadastroLeilaoPage {
@@ -10,4 +11,12 @@ public class CadastroLeilaoPage {
     }
 
 
+    public LeiloesPage cadastrarLeilao(String nome, String valor, String dataAbertura) {
+        browser.findElement(By.id("nome")).sendKeys(nome);
+        browser.findElement(By.id("valorInicial")).sendKeys(valor);
+        browser.findElement(By.id("dataAbertura")).sendKeys(dataAbertura);
+
+        browser.findElement(By.id("button-submit")).submit();
+        return new LeiloesPage(browser);
+    }
 }
--- src/test/java/br/com/alura/leilao/leiloes/LeiloesPage.java
+++ src/test/java/br/com/alura/leilao/leiloes/LeiloesPage.java
@@ -1,6 +1,8 @@
 package br.com.alura.leilao.leiloes;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LeiloesPage {
@@ -20,5 +22,16 @@ public class LeiloesPage {
     public CadastroLeilaoPage carregarFormulario() {
         this.browser.navigate().to(URL_LEILOES_FORM);
         return new CadastroLeilaoPage(browser);
+    }
+
+    public boolean isLeilaoCadastrado(String nome, String valor, String data) {
+        WebElement element = browser.findElement(By.cssSelector("#tabela-leiloes tbody tr:last-child"));
+        WebElement colunaNome = element.findElement(By.cssSelector("td:nth-child(1)"));
+        WebElement colunaData = element.findElement(By.cssSelector("td:nth-child(2)"));
+        WebElement colunaValor = element.findElement(By.cssSelector("td:nth-child(3)"));
+
+        return colunaNome.getText().equals(nome)
+                && colunaData.getText().equals(data)
+                && colunaValor.getText().equals(valor);
     }
 }
